0a663447a73f2b250374740314dfbc60b58f9f7f
Thomas Hänig <thomas.haenig@akquinet.de>
2018-11-05 13:26:02 +0100

    remove .js

--- src/model/app.ts
+++ src/model/app.ts
@@ -1,5 +1,8 @@
+//#region IMPORTS
 import * as http from "http";
 import * as fs from "fs";
+import { isNullOrUndefined } from "util";
+//#endregion
 
 interface IAppConfig {
     hostnameServer: string;
@@ -19,7 +22,6 @@ export class App {
     private url: string;
 
     constructor(config: IAppConfig, global: EngineAPI.IGlobal, appname: string, url: string) {
-        console.log("constructor of App called");
         this.global = global;
         this.appname = appname;
         this.config = config;
@@ -55,84 +57,54 @@ export class App {
 
             let postData = fs.readFileSync("./src/assets/ExecutiveDashboard.xlsx");
             const fileId: string = this._guid();
-            const jsonFile = {
-                "tasks": [
-                    {
-                        "reports": [
-                            {
-                                "general": {
-                                    "cleanupTimeOut": 10,
-                                    "timeout": 900,
-                                    "errorRepeatCount": 2
-                                },
-                                "template": {
-                                    "input": "ExecutiveDashboard.xlsx",
-                                    "output": "='Report_'&only(Region)&'_'&max([Fiscal Year])&'.pdf'",
-                                    "selections": [
-                                        {
-                                            "type": "static",
-                                            "name": "Fiscal Year",
-                                            "values": "2014"
-                                        },
-                                        {
-                                            "type": "dynamic",
-                                            "name": "Region"
-                                        }
-                                    ]
-                                },
-                                "distribute": {},
-                                "connections": [
-                                    {
-                                        "serverUri": this.url.split("/app/engineData")[0],
-                                        "app": "engineData",
-                                        "identities": [this.url.split("identity/")[1]],
-                                        "sslValidThumbprints": [
-                                            {
-                                                "url": "https://nb-fc-208000/ser",
-                                                "thumbprint": "e0e1b550a72365bdbf2aa9a0c5ecc320e35b5985"
-                                            }
-                                        ],
-                                        "credentials": {
-                                            "type": "NONE"
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
-                "uploadGuids": [fileId]
+            const serJson = this.createSERJson(fileId);
+
+            const headers = {
+                    "SerFilename": "ExecutiveDashboard.xlsx"
             };
 
-            const options = {
-                hostname: this.config.hostnameServer,
-                port: 11271,
-                path: `/api/v1/file/${fileId}?filename=ExecutiveDashboard.xlsx`,
-                method: "POST",
-                headers: {
-                    "Content-Type": "text/plain"
-                }
-            };
+            this.sendRequest("POST", `/api/v1/file/${fileId}`, headers, postData)
+                .then((data) => {
+                    let id = JSON.parse(data);
 
-            let id = "";
-            const req = http.request(options, (res) => {
-                console.log(`STATUS: ${res.statusCode}`);
-                console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-                res.setEncoding("utf8");
-                res.on("data", (resId) => {
-                    id = resId;
-                });
-                res.on("end", () => {
-                    this.callTaskPost(jsonFile);
-                    console.log("No more data in response.");
-                });
-            });
+                    return this.sendRequest("POST", `/api/v1/task`, null, serJson);
+                })
+                .then((data) => {
+                    console.log(data);
+                    const taskId
+                })
+                .catch(error => reject(error));
 
-            req.on("error", (e) => {
-                console.error(`problem with request: ${e.message}`);
-            });
-            req.write(postData);
-            req.end();
+
+            // const options = {
+            //     hostname: this.config.hostnameServer,
+            //     port: 11271,
+            //     path: `/api/v1/file/${fileId}?filename=ExecutiveDashboard.xlsx`,
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "text/plain"
+            //     }
+            // };
+
+            // let id = "";
+            // const req = http.request(options, (res) => {
+            //     console.log(`STATUS: ${res.statusCode}`);
+            //     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            //     res.setEncoding("utf8");
+            //     res.on("data", (resId) => {
+            //         id = resId;
+            //     });
+            //     res.on("end", () => {
+            //         this.callTaskPost(jsonFile);
+            //         console.log("No more data in response.");
+            //     });
+            // });
+
+            // req.on("error", (e) => {
+            //     console.error(`problem with request: ${e.message}`);
+            // });
+            // req.write(postData);
+            // req.end();
 
         });
     }
@@ -284,5 +256,105 @@ export class App {
             console.error(`problem with request: ${e.message}`);
         });
         req.end();
+    }
+
+    private sendRequest(methode: "GET" | "POST", path: string, headers: any, postData?: any): Promise<any> {
+
+        return new Promise((resolve, reject) => {
+
+            let options: http.RequestOptions = {
+                hostname: this.config.hostnameServer,
+                port: 11271,
+                path: path,
+                method: methode,
+            };
+
+            if (isNullOrUndefined(headers)) {
+                options = { ...options,
+                    headers: headers
+                };
+            }
+
+            const request = http.request(options, (response) => {
+                const data = [];
+
+                response.on("data", (chunk) => {
+                    console.log("chunk", chunk);
+                    data.push(chunk);
+                });
+
+                response.on("end", () => {
+                    resolve(data);
+                });
+            });
+
+            request.on("error", (error) => {
+                reject(error);
+            });
+
+            switch (methode) {
+                case "POST":
+
+                    request.write(postData);
+                    request.end();
+                    break;
+
+                default:
+                    request.end();
+                    break;
+            }
+
+        });
+    }
+
+    private createSERJson(fileId: string): Object {
+        return {
+            "tasks": [
+                {
+                    "reports": [
+                        {
+                            "general": {
+                                "cleanupTimeOut": 10,
+                                "timeout": 900,
+                                "errorRepeatCount": 2
+                            },
+                            "template": {
+                                "input": "ExecutiveDashboard.xlsx",
+                                "output": "='Report_'&only(Region)&'_'&max([Fiscal Year])&'.pdf'",
+                                "selections": [
+                                    {
+                                        "type": "static",
+                                        "name": "Fiscal Year",
+                                        "values": "2014"
+                                    },
+                                    {
+                                        "type": "dynamic",
+                                        "name": "Region"
+                                    }
+                                ]
+                            },
+                            "distribute": {},
+                            "connections": [
+                                {
+                                    "serverUri": this.url.split("/app/engineData")[0],
+                                    "app": "engineData",
+                                    "identities": [this.url.split("identity/")[1]],
+                                    "sslValidThumbprints": [
+                                        {
+                                            "url": "https://nb-fc-208000/ser",
+                                            "thumbprint": "e0e1b550a72365bdbf2aa9a0c5ecc320e35b5985"
+                                        }
+                                    ],
+                                    "credentials": {
+                                        "type": "NONE"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            "uploadGuids": [fileId]
+        };
     }
 }
